84718db60c6bc70ef8d5839846e4cb7bc901ed16
jartsanull <langinenj1@gmail.com>
2024-08-15 15:40:31 +0300

    lol

--- Spotlight_Stream/Spotlight_Stream/Models/User.cs
+++ Spotlight_Stream/Spotlight_Stream/Models/User.cs
@@ -4,6 +4,9 @@ namespace Spotlight_Stream.Models
 {
     public class User{
 
-        private int Id { get; set; }
+        private int Id;
+        private string username;
+        private string password;
+        private string email;
     }
 }
