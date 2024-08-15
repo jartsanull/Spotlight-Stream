f0538c8470ab812852d5a9c9077a765d54c22ae5
jartsanull <langinenj1@gmail.com>
2024-08-15 16:02:34 +0300

    Update User.cs

--- Spotlight_Stream/Spotlight_Stream/Models/User.cs
+++ Spotlight_Stream/Spotlight_Stream/Models/User.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Spotlight_Stream.Models
 {
     public class User{
 
+        [Key]
         private int Id;
+
         private string username {  get; set; }
         private string password { get; set; }
         private string email { get; set; }
