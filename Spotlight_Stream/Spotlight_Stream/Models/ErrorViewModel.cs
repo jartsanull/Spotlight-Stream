fc165235c290e810298c78f18140ac455efa58f8
jartsanull <langinenj1@gmail.com>
2024-08-15 15:21:14 +0300

    lol

--- Spotlight_Stream/Spotlight_Stream/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Spotlight_Stream.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
