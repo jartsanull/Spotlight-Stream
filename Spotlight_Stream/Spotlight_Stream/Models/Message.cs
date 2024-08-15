dca8231597adefe3b1dd598ce6e82cb082fde7fa
jartsanull <langinenj1@gmail.com>
2024-08-15 16:06:38 +0300

    Update Message.cs

--- Spotlight_Stream/Spotlight_Stream/Models/Message.cs
+++ Spotlight_Stream/Spotlight_Stream/Models/Message.cs
@@ -1,6 +1,23 @@
-﻿namespace Spotlight_Stream.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Spotlight_Stream.Models
 {
-    public class Message
-    {
+    public class Message{
+
+        [Key]
+        private int Id;
+
+        private string sender { get; set; }
+        private string content { get; set; }
+
+        public Message()
+        {
+        }
+
+        public Message(string sender, string content){
+            this.sender = sender;
+            this.content = content;
+        }
     }
 }
