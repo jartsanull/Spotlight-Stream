8eebdf4eb52eea3ef23a0f954c7912ae83cfd1ab
jartsanull <langinenj1@gmail.com>
2024-08-22 21:38:08 +0300

    Update MessageRepository.cs

--- Spotlight_Stream/Spotlight_Stream/Repositories/MessageRepository.cs
+++ Spotlight_Stream/Spotlight_Stream/Repositories/MessageRepository.cs
@@ -6,13 +6,23 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Spotlight_Stream.Repositories{
-    public class MessageRepository : IMeseRepo{
+    public class MessageRepository : IMeseRepo
+    {
         
         private readonly ApplicationDbContext _context;
 
-        public MessageRepository(ApplicationDbContext context)
-        {
+        public MessageRepository(ApplicationDbContext context){
             _context = context;
+        }
+
+        public async Task<Message> GetMessageByIdAsync(long id){
+            return await _context.Messages.FindAsync(id);
+        }
+
+
+        public async Task<List<Message>> GetMessagesBySenderAsync(string sender){
+            return await _context.Messages.Where(m => m.Sender == sender).
+                ToListAsync();
         }
 
 
