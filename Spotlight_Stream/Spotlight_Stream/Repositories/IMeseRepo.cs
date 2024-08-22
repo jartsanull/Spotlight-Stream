5a73642db258bf86f37a0487494a8c73b9250de2
jartsanull <langinenj1@gmail.com>
2024-08-22 21:30:00 +0300

    another class

---
+++ Spotlight_Stream/Spotlight_Stream/Repositories/IMeseRepo.cs
@@ -0,0 +1,15 @@
+﻿using Spotlight_Stream.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spotlight_Stream.Repositories
+{
+    public interface IMeseRepo
+    {
+        Task<Message> GetMessageByIdAsync(long id);
+        Task<List<Message>> GetMessagesBySenderAsync(string sender);
+        Task AddMessageAsync(Message message);
+        Task UpdateMessageAsync(Message message);
+        Task DeleteMessageAsync(long id);
+    }
+}
--- Spotlight_Stream/Spotlight_Stream/obj/Debug/net8.0/Spotlight_Stream.AssemblyInfo.cs
+++ Spotlight_Stream/Spotlight_Stream/obj/Debug/net8.0/Spotlight_Stream.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spotlight_Stream")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566798abedd840a1149b1c82d6193b5f9a5b1fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e314f27b272bf600856ebdfc41589e9b8a83df19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spotlight_Stream")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spotlight_Stream")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
