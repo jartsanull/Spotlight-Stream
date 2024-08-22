d67db49040dc5120c60984151ca0832a838ad1c5
jartsanull <langinenj1@gmail.com>
2024-08-22 21:42:30 +0300

    Update MessageRepository.cs

--- Spotlight_Stream/Spotlight_Stream/Repositories/MessageRepository.cs
+++ Spotlight_Stream/Spotlight_Stream/Repositories/MessageRepository.cs
@@ -25,6 +25,28 @@ namespace Spotlight_Stream.Repositories{
                 ToListAsync();
         }
 
+        public async Task AddMessageAsync(Message message)
+        {
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+        }
 
+        public async Task UpdateMessageAsync(Message message)
+        {
+            _context.Messages.Update(message);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMessageAsync(long id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+            if (message != null)
+            {
+                _context.Messages.Remove(message);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
+
 }
+
