772f5e8442d37c291280a5f584a3e831862521a0
jartsanull <langinenj1@gmail.com>
2024-08-22 21:50:29 +0300

    Update User.cs

--- Spotlight_Stream/Spotlight_Stream/Models/User.cs
+++ Spotlight_Stream/Spotlight_Stream/Models/User.cs
@@ -6,7 +6,7 @@ namespace Spotlight_Stream.Models
     public class User{
 
         [Key]
-        private long Id;
+        public long Id;
 
         public string Username {  get; set; }
         public string Password { get; set; }
