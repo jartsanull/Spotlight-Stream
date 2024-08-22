78136995e26f18e23497c24fa8d9942954f0758e
jartsanull <langinenj1@gmail.com>
2024-08-22 21:32:40 +0300

    Update MessageRepository.cs

--- Spotlight_Stream/Spotlight_Stream/Repositories/MessageRepository.cs
+++ Spotlight_Stream/Spotlight_Stream/Repositories/MessageRepository.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Spotlight_Stream.Data;
+using Spotlight_Stream.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Spotlight_Stream.Repositories
-{
-    public class MessageRepository
-    {
+namespace Spotlight_Stream.Repositories{
+    public class MessageRepository : IMeseRepo{
+        
         private readonly ApplicationDbContext _context;
 
         public MessageRepository(ApplicationDbContext context)
