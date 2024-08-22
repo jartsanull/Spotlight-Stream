248ab1378cae2764b1a5fb397f5020d12dfed7b7
jartsanull <langinenj1@gmail.com>
2024-08-22 21:30:20 +0300

    Update IMeseRepo.cs

--- Spotlight_Stream/Spotlight_Stream/Repositories/IMeseRepo.cs
+++ Spotlight_Stream/Spotlight_Stream/Repositories/IMeseRepo.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Spotlight_Stream.Repositories
-{
-    public interface IMeseRepo
-    {
+namespace Spotlight_Stream.Repositories{
+    public interface IMeseRepo{
         Task<Message> GetMessageByIdAsync(long id);
         Task<List<Message>> GetMessagesBySenderAsync(string sender);
         Task AddMessageAsync(Message message);
