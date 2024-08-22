2a8520e1b2cf2b30868ea4799d39527113c61f1a
jartsanull <langinenj1@gmail.com>
2024-08-22 21:18:54 +0300

    Update ApplicationDbContext.cs

--- Spotlight_Stream/Spotlight_Stream/Data/ApplicationDbContext.cs
+++ Spotlight_Stream/Spotlight_Stream/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ using Spotlight_Stream.Models;
 
 namespace Spotlight_Stream.Data
 {
-    // Inherit from IdentityDbContext to include ASP.NET Core Identity tables and functionality
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
@@ -13,7 +12,6 @@ namespace Spotlight_Stream.Data
         {
         }
 
-        // Define DbSet properties for your custom entities
         public DbSet<Message> Messages { get; set; }
         public DbSet<User> Users { get; set; }
     }
